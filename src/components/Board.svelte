1c8de6469a42634d68bce5d4d4c1a6d082d1a680
Aaron <AJ@jonk.name>
2022-06-18 17:24:15 +0200

    board & piece components

---
+++ src/components/Board.svelte
@@ -0,0 +1,45 @@
+<script>
+import Piece from "./Piece.svelte";
+
+</script>
+
+<div class="wrapper">
+    <div class="board">
+        {#each [...Array(10).keys()] as i}
+            {#each [...Array(10).keys()] as ii}
+                <div class="tile" style="{i % 2 == ((ii + 1) % 2) ? 'background: #80ed9d' : ''}"/>
+            {/each} 
+        {/each}
+
+
+    </div>
+</div>
+
+<style>
+
+div.wrapper {
+    width: 1050px;
+    height: 1050px;
+    position: relative;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 100px;
+}
+
+div.wrapper > div.board {
+    border: #e0a212 2px solid;
+    background: white;
+    width: 1000px;
+    height: 1000px;
+    display: flex;
+    flex-wrap: wrap;
+    box-shadow: 10px 10px 5px 5px #80ED9D;
+    position: relative;
+}
+
+div.wrapper > div.board > div.tile {
+    width: 100px;
+    height: 100px;
+}
+
+</style>
---
+++ src/components/Piece.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let posX = 0;
+    export let posY = 0;
+    export let isKing = false;
+    export let isBlack = false;
+
+    export let onClick = () => {};
+
+    var color: string;
+    if (isBlack) {
+        color = '#000';
+    } else {
+        color = '#e0e0e0';
+    }
+</script>
+
+<div on:click={onClick} style="--theme-color: {color}; left: {posX * 100 + 10}px; top: {posY * 100 + 10}px; {isKing ? 'background: linear-gradient(135deg, #edcb0c 50%, ' + color + ' 50%);' : ''}"/>
+
+<style>
+    div {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+        background: var(--theme-color);
+        position: absolute;
+        cursor: pointer;
+    }
+</style>
